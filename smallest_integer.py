7906d7f2991118b607062cf48c4416f0091b9206
bennydan7 <owusub243@gmail.com>
2025-02-07 00:39:33 +0000

    update

--- smallest_integer.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def smallestEvenMultiple(self, n: int) -> int:
-        for i in range(1, (n * 2) + 1 ):
-            if i % 2 == 0 and i % n == 0:
-                return i
-
-
-        

c5a6f538b10d1bad763e33756874683fa8162d61
Bernard Asiedu Owusu-Dankwah <owusub243@gmail.com>
2025-04-06 18:12:33 +0000

    replace_elements

---
+++ leetcode/replace_elements.py
@@ -0,0 +1,16 @@
+def replace_elements(arr):
+    n = len(arr)
+    if n == 0:
+        return []
+    max_element = -1
+
+    for i in range(n-1,-1,-1):
+        temp = arr[i]
+        arr[i] = max_element
+        if temp > max_element:
+            max_element = temp
+    
+    return arr
+    
+
+print(replace_elements(arr = [17,18,5,4,6,1]))

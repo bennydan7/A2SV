579fd2641d7f119d48953938e463cd4a798711d5
Bernard Asiedu Owusu-Dankwah <owusub243@gmail.com>
2025-05-02 15:25:19 +0000

    pancaKE SORTING

--- leetcode/sorting/pancake_sorting.py
+++ leetcode/sorting/pancake_sorting.py
@@ -1,2 +1,27 @@
-def is_greater(a.b):
-    
+def pancake_sorting(arr):
+
+    def flip(end):
+        start = 0 
+        while start < end:
+            arr[start],arr[end] = arr[end],arr[start]
+            start += 1
+            end -= 1
+    n = len(arr)
+    output = []
+
+    for i in range(n-1,-1,-1):
+        max_element = i
+        for j in range(i,-1,-1):
+            if arr[j] > arr[max_element]:
+                max_element = j
+        if max_element != i:
+            flip(max_element)
+            flip(i)
+            output.append(max_element + 1)
+            output.append(i + 1)
+    return output
+
+
+print(pancake_sorting(arr=[3,2,4,1]))
+print(pancake_sorting(arr=[1,3,2]))
+

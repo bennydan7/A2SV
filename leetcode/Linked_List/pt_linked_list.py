18b8d55316654ad26335becd63ba34b902a73ff8
Bernard Asiedu Owusu-Dankwah <owusub243@gmail.com>
2025-05-04 16:02:03 +0000

    recap

---
+++ leetcode/Linked_List/pt_linked_list.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,value=None):
+        self.value = value
+        self.next = None
+
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+    
+
+singlyLinkedList = SLinkedList()
+node1 = Node(1)
+node2 = Node(2)
+
+singlyLinkedList.head = node1
+singlyLinkedList.head.next = node2
+singlyLinkedList.tail = node2

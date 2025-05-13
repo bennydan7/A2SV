09dfc2e6de29d0c06a70040606f129cb6d715908
Bernard Asiedu Owusu-Dankwah <owusub243@gmail.com>
2025-05-13 10:33:10 +0000

    absolute path

--- leetcode/Stacks,Queues/Absolute_path.py
+++ leetcode/Stacks,Queues/Absolute_path.py
@@ -1,1 +1,12 @@
-# def simplify_path():
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        parts = path.split('/')
+
+        for element in parts:
+            if element == "..":
+                if stack:
+                    stack.pop()
+            elif element and element != ".":
+                stack.append(element)
+        return "/" + ("/").join(stack)
